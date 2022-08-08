a06478584490c0341f063a5105805e505706afa7
Chris <554650126@qq.com>
2022-08-08 23:45:31 +0800

    增加若干个传感器的采集指令速查表

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    title: "采集指令速查表",
+    themeConfig: {
+        logo: "/assets/img/logo.png",
+        sidebar: [
+            "/",
+            {
+                title: "位移计",
+                sidebarDepth: 0,
+                children: [
+                    { path: "/dis/zheDa", title: "浙达位移" },
+                    { path: "/dis/jiGuang", title: "激光位移" }
+                ]
+            },
+            {
+                title: "风速仪",
+                sidebarDepth: 0,
+                children: [
+                    { path: "/wind/jinMa", title: "金码二维风速仪" },
+                    { path: "/wind/renKe", title: "建大仁科二维风速仪" }
+                ]
+            },
+            {
+                title: "温度计",
+                sidebarDepth: 0,
+                children: [
+                    { path: "/temperature/kunHang", title: "鲲航数字温度" }
+                ]
+            }
+        ],
+    }
+};
